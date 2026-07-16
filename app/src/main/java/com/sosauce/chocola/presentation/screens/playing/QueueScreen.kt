87440201ae8d77e00ffae3501f9afa92b233d076
Elnix90 <elnix91@proton.me>
2026-07-16 08:46:55 +0200

    fix: corrected crash when entering QueueScreen.kt due to key not being bundle-able, closes #301

--- app/src/main/java/com/sosauce/chocola/presentation/screens/playing/QueueScreen.kt
+++ app/src/main/java/com/sosauce/chocola/presentation/screens/playing/QueueScreen.kt
@@ -17,14 +17,11 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.util.fastMap
 import com.sosauce.chocola.R
 import com.sosauce.chocola.data.states.MusicState
 import com.sosauce.chocola.domain.actions.PlayerActions
@@ -34,7 +31,6 @@ import com.sosauce.chocola.utils.selfAlignHorizontally
 import sh.calvin.reorderable.ReorderableItem
 import sh.calvin.reorderable.rememberReorderableLazyListState
 import kotlin.uuid.ExperimentalUuidApi
-import kotlin.uuid.Uuid
 
 @Composable
 fun QueueScreen(
@@ -71,7 +67,7 @@ fun QueueScreen(
         ) {
             items(
                 items = musicState.loadedMedias,
-                key = { it.mediaItem}
+                key = { it.mediaItem.mediaId }
             ) { track ->
                 ReorderableItem(
                     state = reorderableLazyListState,
@@ -119,10 +115,7 @@ fun QueueScreen(
                         }
                     )
                 }
-
             }
         }
-
     }
-
 }
